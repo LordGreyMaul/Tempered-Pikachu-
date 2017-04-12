d131de74be8d2d919b373988536aa23330078ed0
Graham <Graham@lendyfinanace.com>
2017-04-12 15:54:44 +0100

    sass and splash started
    added new sass sheets and started the home page

--- views/index.view.php
+++ views/index.view.php
@@ -4,15 +4,16 @@
 <!-- Fixed top nav -->
 <?php include('partials/nav.php'); ?>
 <!-- Page Content -->
-<div class="container">
-	<div class="row box">
-		<div class="col-md-12 text-center">
-			<img src="/resources/img/logo.png" alt="Og Pokemon" class="center-block img-responsive">
-			<p>A simple to use tool for finding all those Pokemon Cards</p>
+<div class="parallax-window" data-parallax="scroll" data-image-src="/resources/img/background1.jpg">
+	<div class="container">
+		<div class="row splash">
+			<div class="col-md-12 text-center">
+				<img src="/resources/img/logo.png" alt="Og Pokemon" class="center-block img-responsive">
+				<h2>A simple to use tool for finding all Pokemon Cards</h2>
+			</div>
 		</div>
 	</div>
 </div>
-<div class="parallax-window" data-parallax="scroll" data-image-src="/resources/img/background1.jpg"></div>
 <!-- End of Page Content -->
 
 <!-- Script locations and closing tags -->
