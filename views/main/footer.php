d70e980d83665d961af3f973b7ad977737275e00
Graham <Graham@lendyfinanace.com>
2017-04-14 06:34:04 +0100

    changes to footer
    added new footer etc

--- views/main/footer.php
+++ views/main/footer.php
@@ -1,3 +1,19 @@
+<div class="container-fluid footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <img src="/resources/img/footer.png" alt="Footer Jiggly puff" class="img-responsive">
+                </div>
+                <div class="col-md-4">
+                    <div class="fb-page" data-href="https://www.facebook.com/OGPokemon-1854643698143317/" data-tabs="timeline" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/OGPokemon-1854643698143317/" class="fb-xfbml-parse-ignore">
+                    <a href="https://www.facebook.com/OGPokemon-1854643698143317/">OGPokemon</a>
+                    </blockquote></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
